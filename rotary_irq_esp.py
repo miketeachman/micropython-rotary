490ce36a9e1daa760b634e5ad47968778b537521
Udo Waechter <udo@maketank.net>
2019-02-20 13:25:36 +0100

    ESP32 can use all GPIOs

--- rotary_irq_esp.py
+++ rotary_irq_esp.py
@@ -3,24 +3,27 @@
 # https://opensource.org/licenses/MIT
 
 # Platform-specific MicroPython code for the rotary encoder module
-# ESP8266 implementation
+# ESP8266/ESP32 implementation
+# ESP32 has IRQs on all Pins
 
 # Documentation:
 #   https://github.com/MikeTeachman/micropython-rotary
 
 from machine import Pin
 from rotary import Rotary
+from sys import platform
 
-_esp8266_rotary_pins = [4, 5, 12, 13, 14]
+_rotary_pins = [4, 5, 12, 13, 14]
 
 class RotaryIRQ(Rotary): 
     
     def __init__(self, pin_num_clk, pin_num_dt, min_val=0, max_val=10, reverse=False, range_mode = Rotary.RANGE_UNBOUNDED):
         
-        if not pin_num_clk in _esp8266_rotary_pins:
-            raise ValueError
-        if not pin_num_dt in _esp8266_rotary_pins:
-            raise ValueError            
+        if platform != 'esp32':
+            if not pin_num_clk in _rotary_pins:
+                raise ValueError
+            if not pin_num_dt in _rotary_pins:
+                raise ValueError            
 
         self._pin_clk = Pin(pin_num_clk, Pin.IN)
         self._pin_dt = Pin(pin_num_dt, Pin.IN)
