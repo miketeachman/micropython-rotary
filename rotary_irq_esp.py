fbeb67a33b41b75936afd9399a5a1c6b41ae94f9
Udo Waechter <udo@maketank.net>
2019-02-21 18:31:31 +0100

    adding an esp32 blacklist with strapping pins

--- rotary_irq_esp.py
+++ rotary_irq_esp.py
@@ -13,17 +13,23 @@ from machine import Pin
 from rotary import Rotary
 from sys import platform
 
-_rotary_pins = [4, 5, 12, 13, 14]
+_esp8266_whitelist_pins = [4, 5, 12, 13, 14]
+_esp32_blacklist_pins = [0, 2, 4, 5, 12, 15]
 
 class RotaryIRQ(Rotary): 
     
     def __init__(self, pin_num_clk, pin_num_dt, min_val=0, max_val=10, reverse=False, range_mode = Rotary.RANGE_UNBOUNDED):
         
-        if platform != 'esp32':
-            if not pin_num_clk in _rotary_pins:
-                raise ValueError
-            if not pin_num_dt in _rotary_pins:
-                raise ValueError            
+        if platform == 'esp32':
+            if pin_num_clk in _esp32_blacklist_pins:
+                raise ValueError('%s: Pin %d not allowed. Blacklist: %s' % (platform, pin_num_clk,_esp32_blacklist_pins))
+            if pin_num_dt in _esp32_blacklist_pins:
+                raise ValueError('%s: Pin %d not allowed. Blacklist: %s' % (platform, pin_num_dt,_esp32_blacklist_pins))
+        else:
+            if not pin_num_clk in _esp8266_whitelist_pins:
+                raise ValueError('%s: Pin %d not allowed. Whitelist: %s' % (platform, pin_num_dt,_esp8266_whitelist_pins))
+            if not pin_num_dt in _esp8266_whitelist_pins:
+                raise ValueError('%s: Pin %d not allowed. Whitelist: %s' % (platform, pin_num_dt,_esp8266_whitelist_pins))            
 
         self._pin_clk = Pin(pin_num_clk, Pin.IN)
         self._pin_dt = Pin(pin_num_dt, Pin.IN)
